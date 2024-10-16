ae4df4e5c866941adf369a2cea9ef02e0e5a1129
Keith Adams <kma@pebblebed.com>
2024-10-16 18:06:08 +0000

    Model a compile-time value sequence for tensor dims.

--- tests/tensor_tests.cpp
+++ tests/tensor_tests.cpp
@@ -5,8 +5,43 @@
 using namespace trails;
 using namespace trails::detail;
 
-TEST(TensorTests, IntSequence) {
-    EXPECT_EQ(int_sequence<>::length, 0);
+TEST(TensorTests, ValSequence) {
+    EXPECT_EQ(val_sequence<double>::length, 0);
+    EXPECT_EQ((val_sequence<double, 1.0>::length), 1);
+    EXPECT_EQ((val_sequence<double, 1.0, 2.0>::length), 2);
+    EXPECT_EQ((val_sequence<double, 1.0, 2.0, 3.0>::length), 3);
+
+    typedef val_sequence<int, 12, 7, -43> vals_3d;
+    EXPECT_EQ(vals_3d::length, 3);
+    EXPECT_EQ((vals_3d::get<0>()), 12);
+    EXPECT_EQ((vals_3d::get<1>()), 7);
+    EXPECT_EQ((vals_3d::get<2>()), -43);
+
+    typedef val_sequence<int, 12, 7, -43, 1> vals_4d;
+    EXPECT_EQ(vals_4d::length, 4);
+    EXPECT_EQ(vals_4d::get<0>(), 12);
+    EXPECT_EQ(vals_4d::get<1>(), 7);
+    EXPECT_EQ(vals_4d::get<2>(), -43);
+    EXPECT_EQ(vals_4d::get<3>(), 1);
 }
 
+TEST(TensorTests, ValSequenceIndexing) {
+    EXPECT_EQ((val_sequence<int, 12, 7, -43, 1>::get<0>()), 12);
+    EXPECT_EQ((val_sequence<int, 12, 7, -43, 1>::get<1>()), 7);
+    EXPECT_EQ((val_sequence<int, 12, 7, -43, 1>::get<2>()), -43);
+    EXPECT_EQ((val_sequence<int, 12, 7, -43, 1>::get<3>()), 1);
+}
 
+TEST(TensorTests, Comparison){
+    EXPECT_TRUE(Tensor<>::compare_sizes(torch::IntArrayRef{}));
+    EXPECT_FALSE(Tensor<>::compare_sizes(torch::IntArrayRef{1}));
+    EXPECT_FALSE(Tensor<1>::compare_sizes(torch::IntArrayRef{}));
+
+    EXPECT_TRUE(Tensor<1>::compare_sizes(torch::IntArrayRef{1}));
+    EXPECT_FALSE(Tensor<1>::compare_sizes(torch::IntArrayRef{2}));
+
+    EXPECT_TRUE((Tensor<1, 2>::compare_sizes(torch::IntArrayRef{1, 2})));
+    EXPECT_FALSE((Tensor<1, 2>::compare_sizes(torch::IntArrayRef{1, 3})));
+    EXPECT_FALSE((Tensor<1, 2>::compare_sizes(torch::IntArrayRef{2, 2})));
+    EXPECT_FALSE((Tensor<2, 3>::compare_sizes(torch::IntArrayRef{2, 3, 4})));
+}
--- trails.hpp
+++ trails.hpp
@@ -16,84 +16,83 @@ namespace trails {
 
 namespace detail {
 
-template<typename I=size_t, I ...N> struct int_sequence {};
+template<typename I=size_t, I ...N> struct val_sequence {};
 // Specialization for 0-D
-template<typename I>
-struct int_sequence<I> {
-    constexpr static I length = 0;
-    typedef std::tuple<> tuple_type;
+template<typename V>
+struct val_sequence<V> {
+    constexpr static size_t length = 0;
+    typedef std::tuple<> tuple_t;
+    constexpr static tuple_t values = tuple_t();
 };
 
-template<typename I, I N, I... Rest>
-struct int_sequence<I, N, Rest...> {
-    constexpr static size_t length = 1 + int_sequence<I, Rest...>::length;
+template<typename V, V N, V... Rest>
+struct val_sequence<V, N, Rest...> {
+    typedef val_sequence<V, Rest...> next_t;
+    constexpr static size_t length = sizeof...(Rest) + 1;
     // Utility to cons up a tuple type
     template<typename ... input_t>
     using tuple_cat_t = decltype(std::tuple_cat(std::declval<input_t>()...));
-    typedef int_sequence<I, Rest...>::tuple_type next_tuple_t;
-    typedef tuple_cat_t<std::tuple<I>, next_tuple_t> tuple_type;
-    // typedef tuple_cat_t<std::tuple<I>, int_sequence<I, Rest...>::tuple_type> tuple_type;
-#if 0
-    constexpr static int first = N;
-    constexpr static int last = std::get<length - 1>(values);
-    template<size_t i>
-    struct get {
-        constexpr static int value = std::tuple_element<i - 1, std::tuple<N, Dims...>>::value;
-    };
-#endif
+    typedef tuple_cat_t<std::tuple<V>, typename next_t::tuple_t> tuple_t;
+    constexpr static tuple_t values = std::tuple_cat(std::tuple<V>(N), val_sequence<V, Rest...>::values);
+
+    template<int64_t i>
+    constexpr static V get() {
+        return std::get<size_t(i)>(values);
+    }
+};
+
+// Primary template
+template<typename Seq, int64_t i>
+struct compare_sizes_helper_t {
+    static bool compare(const Seq& seq, torch::IntArrayRef sizes) {
+        return seq.template get<i>() == sizes[i] && compare_sizes_helper_t<Seq, i - 1>::compare(seq, sizes);
+    }
+};
+
+// Specialization for i = -1
+template<typename Seq>
+struct compare_sizes_helper_t<Seq, -1> {
+    static bool compare(const Seq&, torch::IntArrayRef) { return true; }
+};
+
+// Specialization for i = 0
+template<typename Seq>
+struct compare_sizes_helper_t<Seq, 0> {
+    static bool compare(const Seq& seq, torch::IntArrayRef sizes) {
+        return seq.template get<0>() == sizes[0];
+    }
 };
 
 }
 
-#if 0
-// Now a Tensor!
-template<int... Dims> struct Tensor;
-
-// Specialization for 0-D
-template<>
-struct Tensor<> {
-    constexpr static int dims = 0;
-    typedef dims_t = std::tuple<>;
-};
-
-
 // Specialization for concrete dimensions. -1 means "any size"
-template<int N, int... Dims>
-struct Tensor<N, Dims...> {
-    typedef dims_t = std::tuple<int, 
-    static auto size() {
-        return std::tuple_cat(std::tuple<int>(N), NextDim::size());
-    }
-
-    using TupleType = std::tuple<N, Dims...>;
-    constexpr static int k_FirstDim = N;
-    constexpr static int k_LastDim = std::tuple_element<std::tuple_size<TupleType>::value - 1, TupleType>::value;
-    using NextOuterTypeDim = Tensor<N, 
-    using NextDim = Tensor<Dims...>;
-    constexpr static int dims = 1 + NextDim::dims;
+template<int ...Dims>
+struct Tensor {
+    using seq_t = detail::val_sequence<size_t, Dims...>;
+    constexpr static auto dims = seq_t::length;
+    constexpr static auto size = seq_t::values;
 
     Tensor()
-    : t_(torch::randn({N, Dims...})) {}
+    : t_(torch::randn({Dims...})) {}
     Tensor(torch::Tensor t)
     : t_(t) {
         assert(compare_sizes(t.sizes()));
     }
 
-    NextDim& operator[](int i) {
-        return NextDim(t_[i]);
+    static Tensor randn() {
+        return Tensor(torch::randn({Dims...}));
     }
-    private:
-    torch::Tensor t_;
 
-    bool compare_sizes(torch::IntArrayRef sizes) {
+    // Public for testing
+    static bool compare_sizes(torch::IntArrayRef sizes) {
         if (sizes.size() != dims) {
             return false;
         }
-        if (N != -1 && sizes[0] != N) {
-            return false;
-        }
-        return NextDim::compare_sizes(sizes.slice(1));
+        return detail::compare_sizes_helper_t<seq_t, int64_t(dims) - 1>::compare(seq_t(), sizes);
     }
+ 
+    private:
+    torch::Tensor t_;
 };
-#endif
+
 }
